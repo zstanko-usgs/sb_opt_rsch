2520a15ff87a2e6ec60a525270b36c6bc3a7b435
axkent <49050273+axkent@users.noreply.github.com>
2020-06-10 13:19:06 -0700

    updated script to work with files in the source data direcotry

--- meanrain_calculator.R
+++ meanrain_calculator.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(data.table)
 #find annual mean
-rain <- read.csv('rain.csv')
+rain <- read.csv('source_data/rain.csv')
 rain_mean_annual<-aggregate( daily.rain ~ year, rain, mean )
 rain_mean_annual<-rain_mean_annual%>% 
   rename(
@@ -10,7 +10,7 @@ rain_mean_annual<-rain_mean_annual%>%
 #write.csv(rain_mean_annual,'rain_mean_annual.csv')
 
 #find quarterly mean
-rain_quarter <- read.csv('rain_quarters.csv')
+rain_quarter <- read.csv('source_data/rain_quarters.csv')
 rain_mean_quarter<- dcast(setDT(rain_quarter), quarter ~ year, value.var = 'daily.rain', mean)
 #write.csv(rain_mean_quarter,'rain_mean_quarter.csv')
 
